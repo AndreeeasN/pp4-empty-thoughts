767b7af1e9aeb50ced6a82546a48f10a9b86bfb1
Andreas Norberg Bergström <andorojas@gmail.com>
2023-06-10 21:49:49 +0200

    Add basic read functionality to thought items

--- thoughts/models.py
+++ thoughts/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Thought(models.Model):
     """
     Database model for user-submitted thoughts
     """
-    title = models.CharField(max_length=48, null=False, blank=False, default="Untitled Thought")
+    title = models.CharField(
+        max_length=48, null=False, blank=False, default="Untitled Thought")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE)
     content = models.CharField(max_length=280, null=False, blank=False)
--- thoughts/views.py
+++ thoughts/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Thought
 
 # Create your views here.
+
+
 def get_thoughts(request):
-    return render(request, 'thoughts/thoughts.html')
+    thoughts = Thought.objects.all()
+    context = {
+        'thoughts': thoughts
+    }
+
+    return render(request, 'thoughts/view_thoughts.html', context)
