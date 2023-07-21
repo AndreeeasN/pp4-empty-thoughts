a4ae96fd752f64f5f2892b2491df71a58eb204bd
Andreas Norberg Bergström <andorojas@gmail.com>
2023-07-21 17:08:45 +0200

    Style Thought forms using crispy FormHelper

--- thoughts/forms.py
+++ thoughts/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-# from django.forms.widgets import TextInput
 from .models import Thought, Comment, Tag
 from thoughts.widgets import TagWidget, TimeWidget
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, Submit
 
 
 class ThoughtForm(forms.ModelForm):
@@ -18,6 +19,21 @@ class ThoughtForm(forms.ModelForm):
         widget=TimeWidget,
         required=False
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('title', css_class='form-group col-md-8 mb-0 px-1'),
+                Column('time', css_class='form-group col-md-4 mb-0 px-1'),
+                css_class='form-row'
+            ),
+            'content',
+            'tags',
+            'anonymous',
+            Submit('submit', 'Submit Thought')
+        )
 
     class Meta:
         model = Thought
